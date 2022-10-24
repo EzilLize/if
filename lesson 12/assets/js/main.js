1cc569dd35d69469c70b0c58839dc85e534246d0
EzilLize <115159321+EzilLize@users.noreply.github.com>
2022-10-24 15:36:42 +0300

    Add files via upload

---
+++ lesson 12/assets/js/main.js
@@ -0,0 +1,65 @@
+//let num=10;
+//if (num==10) {
+//    alert("That's right!");
+//} else {
+//    alert("Incorrect!");
+//}
+
+//var min = 19;
+//if (min >= 0 && min <= 14) {
+//	alert('В первую четверть.');
+//}
+//else if (min >= 15 && min <= 30) {
+//	alert('Во вторую четверть.');
+//}
+//else if (min >= 31 && min <= 45) {
+//	alert('В третью четверть.');
+//}
+//else if (min >= 46 && min <= 59) {
+//	alert('В четвертую четверть.');
+//}
+
+//var lang = 'en';
+//if (lang == 'ru') {
+//	var arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+//}
+//if (lang == 'en') {
+//	arr = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+//}
+//alert(arr);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a==0) ? `Верно`: `Неверно`;
+//alert(b);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a>0) ? `Correct`: `Incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a<0) ? `Correct`: `Incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a>=0) ? `correct`: `incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a<=0) ? `correct`: `incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между 1,0,-3`,0);
+//let b=(a!=0) ? `correct`: `incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между test,тест,3`,'test');
+//let b=(a=='test') ? `correct`: `incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между "1",1,3`,1);
+//let b=(a==1) ? `correct`: `incorrect`;
+//alert(b);
+
+//let a=prompt(`Введите число между 5,0,-3,2`,1,2,3,4);
+//let b=(a>0, a<5) ? `correct`: `incorrect`;
+//alert(b);
